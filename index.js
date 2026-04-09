38b2f3861b7352fdc05d3941170f5440450d06c3
Satish Pawar <pawarsatish8292@gmail.com>
2026-04-10 04:00:21 +0530

    4 changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Version 3 deployed');
+  res.send('🚀 Version 4 deployed');
 });
 
 app.get('/health', (req, res) => {
